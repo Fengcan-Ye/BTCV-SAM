44a7a3acb92eaad16be01d4af00b4b98d0af0ff8
Fengcan Ye <yfcfloyd@pku.edu.cn>
2023-06-14 23:25:29 +0800

    move batched input generating function to utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import cv2
+from segment_anything.utils.transforms import ResizeLongestSide
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -22,3 +23,46 @@ def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+
+
+def prepare_image(image, transform, device):
+    #print(image.shape)
+    image = transform.apply_image(image)
+    image = torch.as_tensor(image, device=device) 
+    return image.permute(2, 0, 1).contiguous()
+
+
+def batched_input_gen(images, prompts, types, sam):
+    """
+    参数：
+    images:  torch.Tensor [B, H, W, 3] 
+    prompts: list of dictionaries of length B
+             for box prompts: organ_id -> torch.Tensor[xmin, ymin, xmax, ymax]
+             for single point prompts:
+             for multipoint prompts:  
+    types:   list of strings indicating prompt types 
+             each string can be 'box' or 'point'
+    返回：
+    batched_input: list of dictionaries with keys 
+             'image', 'original size', 'point_coords', 'point_labels', 'boxes'
+    """
+    batch_size = images.shape[0]
+    batched_input = []
+    resize_transform = ResizeLongestSide(sam.image_encoder.img_size)
+
+    for img_idx in range(batch_size):
+        input = {'image' : prepare_image(images[img_idx].numpy(), resize_transform, sam.device), 
+                 'original_size' : images.shape[1:3]}
+
+        if types[img_idx] == 'box':
+            bboxes = prompts[img_idx] # dict of organ_id -> bbox 
+            input['boxes'] = resize_transform.apply_boxes_torch(torch.vstack(list(bboxes.values())), 
+                                                                                 input['original_size'])
+        else:
+            # point prompt not implemented
+            raise NotImplementedError
+
+        
+        batched_input.append(input)
+    
+    return batched_input
