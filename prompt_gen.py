fa652c0a6af5e344308408f2c420deb56faee0e8
Fengcan Ye <yfcfloyd@pku.edu.cn>
2023-06-15 14:26:16 +0800

    implemented random single point prompt

--- prompt_gen.py
+++ prompt_gen.py
@@ -3,19 +3,40 @@ import torch
 n_categories = 13
 
 
-def single_point_prompt(labels : torch.Tensor):
+def single_point_prompt(labels : torch.Tensor, device=None):
     """
     参数：
     labels: torch.Tensor[B, H, W]
 
     返回：
-    input_points: [dict of organid : torch.Tensor[N, 2]]
-    input_labels: [dict of organid : torch.Tensor[N, 1]]
+    input_points: [dict of organid : torch.Tensor[2, ]]
     """
 
-    raise NotImplementedError 
+    batchsize = labels.shape[0]
+    if device is None:
+        device = labels.device 
+    
+    input_points = [dict() for _ in range(batchsize)]
 
-def multi_point_prompt(labels : torch.Tensor):
+    for organ_id in range(1, n_categories + 1):
+        # for every organ 
+        organ_mask = labels == organ_id 
+
+        for i in range(batchsize):
+            slice = organ_mask[i]
+
+            if torch.any(slice):
+                segmentation = torch.where(slice) # tuple[tensor, tensor]
+                n_pixels = len(segmentation[0])
+                selected = torch.randint(0, n_pixels, (1,))
+
+                input_points[i][organ_id] = torch.tensor([segmentation[1][selected], segmentation[0][selected]], device=device)
+    
+    return input_points
+
+
+
+def multi_point_prompt(labels : torch.Tensor, device=None):
     raise NotImplementedError
 
 def box_prompt(labels : torch.Tensor, device=None):
