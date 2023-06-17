e9e8c0ea6829a58c8168d0232edfc50a74f91b78
Fengcan Ye <yfcfloyd@pku.edu.cn>
2023-06-18 00:22:21 +0800

    Implemented single random prompt generation

--- prompt_gen.py
+++ prompt_gen.py
@@ -1,9 +1,10 @@
 import torch
+import random
 
 n_categories = 13
 
 
-def point_prompt(labels : torch.Tensor, n_points=1, device=None):
+def point_prompt(labels : torch.Tensor, n_points=1, device=None, single_prompt=False):
     """
     参数：
     labels: torch.Tensor[B, H, W]
@@ -18,32 +19,41 @@ def point_prompt(labels : torch.Tensor, n_points=1, device=None):
     
     input_points = [dict() for _ in range(batchsize)]
 
-    for organ_id in range(1, n_categories + 1):
-        # for every organ 
-        organ_mask = labels == organ_id 
+    organ_id_range = list(range(1, n_categories + 1))
+    if single_prompt:
+        random.shuffle(organ_id_range)
 
-        for i in range(batchsize):
-            slice = organ_mask[i]
+    for i in range(batchsize):
+        # for every image in the batch
+        for organ_id in organ_id_range:
+            # for every organ
+            slice = labels[i] == organ_id 
 
             if torch.any(slice):
-                segmentation = torch.where(slice) # tuple[tensor, tensor]
-                n_pixels = len(segmentation[0])
-                selected = []
+                input_points[i][organ_id] = sample_points(slice, n_points, device)
 
-                while len(selected) < n_points:
-                    randint = torch.randint(0, n_pixels, (1,)).item()
-
-                    if n_pixels < n_points or randint not in selected:
-                        selected.append(randint)
-
-                xs = segmentation[1][selected]
-                ys = segmentation[0][selected]
-
-                input_points[i][organ_id] = torch.vstack([xs, ys]).T.to(device)
+                if single_prompt:
+                    break
     
     return input_points
 
-def box_prompt(labels : torch.Tensor, device=None):
+def sample_points(slice, n_points, device):
+    segmentation = torch.where(slice) # tuple[tensor, tensor]
+    n_pixels = len(segmentation[0])
+    selected = []
+
+    while len(selected) < n_points:
+        randint = torch.randint(0, n_pixels, (1,)).item()
+
+        if n_pixels < n_points or randint not in selected:
+            selected.append(randint)
+
+    xs = segmentation[1][selected]
+    ys = segmentation[0][selected]
+
+    return torch.vstack([xs, ys]).T.to(device)
+
+def box_prompt(labels : torch.Tensor, device=None, single_prompt=False):
     """
     参数：
     labels: torch.Tensor[B, H, W]
@@ -57,21 +67,31 @@ def box_prompt(labels : torch.Tensor, device=None):
         device = labels.device
     input_boxes = [dict() for _ in range(batchsize)]
 
-    for organ_id in range(1, n_categories + 1):
-        # for every organ 
-        organ_mask = labels == organ_id
+    organ_id_range = list(range(1, n_categories + 1))
+    if single_prompt:
+        random.shuffle(organ_id_range)
 
-        for i in range(batchsize):
-            slice = organ_mask[i]
+    for i in range(batchsize):
+        # for every image in the batch
+        for organ_id in organ_id_range:
+            # for every organ
+            slice = labels[i] == organ_id
 
             if torch.any(slice):
-                segmentation = torch.where(slice)
+                input_boxes[i][organ_id] = bounding_box(slice, device)
 
-                xmin = torch.min(segmentation[1])
-                xmax = torch.max(segmentation[1])
-                ymin = torch.min(segmentation[0])
-                ymax = torch.max(segmentation[0])
-
-                input_boxes[i][organ_id] = torch.tensor([xmin, ymin, xmax, ymax], device=device)
+                if single_prompt:
+                    break
     
     return input_boxes
+
+
+def bounding_box(slice, device):
+    segmentation = torch.where(slice)
+
+    xmin = torch.min(segmentation[1])
+    xmax = torch.max(segmentation[1])
+    ymin = torch.min(segmentation[0])
+    ymax = torch.max(segmentation[0])
+
+    return torch.tensor([xmin, ymin, xmax, ymax], device=device)
