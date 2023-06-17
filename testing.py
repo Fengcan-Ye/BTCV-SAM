486cf77c8348d1be01e11a9813604d30c5545199
Fengcan Ye <yfcfloyd@pku.edu.cn>
2023-06-18 03:46:10 +0800

    add validation loss

--- testing.py
+++ testing.py
@@ -1,9 +1,12 @@
 import torch 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import numpy as np
 import tqdm
 from utils import batched_input_gen
 from segment_anything.modeling.sam import Sam
+from sam_btcv import SamBTCV
+from prompt_gen import *
+from metric import *
 
 @torch.no_grad()
 def test_model(validation_loader : DataLoader,
@@ -45,3 +48,56 @@ def test_model(validation_loader : DataLoader,
 
     return dice, mDice
 
+@torch.no_grad()
+def validation_loss(validation_set : Dataset, 
+                    model : SamBTCV, 
+                    p_prompts = [1/3, 1/6, 1/6, 1/3]):
+    validation_loader = DataLoader(validation_set, batch_size=1, shuffle=False)
+    device = model.device
+
+    random_p = np.random.uniform(0, 1, len(validation_loader)).reshape((-1, 1))
+    cum_p = np.cumsum(p_prompts)
+    prompt_indices = ((random_p - cum_p) > 0).sum(axis=1)
+    prompt_generators = [lambda labels, device: box_prompt(labels, device, True), 
+                     lambda labels, device: point_prompt(labels, 2, device, True), 
+                     lambda labels, device: point_prompt(labels, 3, device, True), 
+                     lambda labels, device: point_prompt(labels, 1, device, True)]
+    prompt_types = ['box'] + ['point'] * 3
+
+    focal_losses = []
+    dice_losses = []
+    mse_losses = []
+    cross_entropy_losses = []
+
+    for i, data in enumerate(validation_loader):
+        imgs, labels = data['image'], data['label']
+        prompt_idx = prompt_indices[i]
+        prompt = prompt_generators[prompt_idx](labels, model.device)
+        gt_label = list(prompt[0].keys())[0]
+        batched_input = batched_input_gen(imgs, prompt, [prompt_types[prompt_idx]] * 1, model)
+        batched_output = model(batched_input, False)[0]
+
+        pred_mask = batched_output['masks']
+        iou_prediction = batched_output['iou_predictions']
+        pred_logits = model.postprocess_masks(batched_output['low_res_logits'], batched_input[0]['image'].shape[-2:], 
+                                              batched_input[0]['original_size'])
+        if model.requires_classification:
+            pred_class = batched_output['class_predictions']
+            cross_entropy_losses.append(F.cross_entropy(pred_class[0][0], torch.tensor(gt_label, device=device)))
+
+        gt_mask = (labels == gt_label).to(device)
+        mse_losses.append((IoU(pred_mask[0], gt_mask) - iou_prediction) ** 2)
+        gt_mask = torch.where(gt_mask, 1.0 , 0.0)
+        focal_losses.append(FocalLoss(pred_logits, gt_mask))
+        dice_losses.append(DiceLoss(pred_logits, gt_mask))
+
+    mse_loss = torch.mean(torch.stack(mse_losses))
+    focal_loss = torch.mean(torch.stack(focal_losses))
+    dice_loss = torch.mean(torch.stack(dice_losses))
+    cross_entropy_loss = torch.mean(torch.stack(cross_entropy_losses)) if model.requires_classification \
+                                                                               else torch.tensor([0], device=device)
+
+    total_loss = mse_loss + 20 * focal_loss + dice_loss + cross_entropy_loss
+    
+    return total_loss.item()
+    
